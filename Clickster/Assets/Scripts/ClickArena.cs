4258b959aa1bfb430272472e1702ebce5938cc87
Andy Zinsser <andyznssr@gmail.com>
2014-12-10 13:13:33 -0800

    First commit

---
+++ Clickster/Assets/Scripts/ClickArena.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ClickArena : MonoBehaviour {
+	
+	static int padding = 10;
+	static int buttonHeight = 100;
+	static int boxWidth = Screen.width - padding * 2;
+	static int boxHeight = buttonHeight * 2 + padding * 3;
+	static int boxY = (Screen.height - boxHeight) / 2;
+	static int buttonWidth = boxWidth - padding * 2;
+	
+	int score = 0;
+	float sec = 0.0f;
+	float timeRemaining = 10.0f;
+	
+	
+	void Update () 
+	{   
+		if ( timeRemaining > 0 ) {
+			RunTimer();
+		}
+	}
+	
+	
+	void OnGUI() {
+		
+		GUIStyle buttonStyle = new GUIStyle("button");
+		buttonStyle.fontSize = 32;
+		GUIStyle boxStyle = new GUIStyle("box");
+		boxStyle.fontSize = 38;
+		GUIStyle labelStyle = new GUIStyle("label");
+		labelStyle.fontSize = 18;
+		labelStyle.alignment = TextAnchor.MiddleCenter;
+		
+		GUI.Box(new Rect(padding, boxY, boxWidth, boxHeight), "The Game", boxStyle);
+		GUI.Label(new Rect(0, boxY - padding - labelStyle.fontSize, buttonWidth, buttonHeight), "Score:" + score, labelStyle);
+		GUI.Label(new Rect(0, boxY - padding - labelStyle.fontSize * 2, buttonWidth, buttonHeight), "Time remaining:" + timeRemaining, labelStyle);
+		
+		if ( timeRemaining > 0 ) {
+			if(GUI.Button(new Rect(padding * 2, buttonHeight + boxY + padding, buttonWidth, buttonHeight), "click!!!", buttonStyle)) {
+				score++;
+			}
+		}
+		
+		if ( timeRemaining == 0 ) {
+			if(GUI.Button(new Rect(padding * 2, buttonHeight * 2 + boxY + padding, buttonWidth, buttonHeight), "Back", buttonStyle)) {
+				Application.LoadLevel("MainMenu");
+			}
+		}
+	}
+	
+	void RunTimer() {
+		sec += Time.deltaTime;
+		if (sec >= 1) {
+			timeRemaining -= Mathf.Floor(sec);
+			sec = 0;	
+		}
+	}
+}
---
+++ Clickster/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour {
+	
+	private static int padding = 10;
+	private static int buttonHeight = 100;
+	private static int boxWidth = Screen.width - padding * 2;
+	private static int boxHeight = buttonHeight * 6 + padding * 5;
+	private static int boxY = (Screen.height - boxHeight) / 2;
+	private static int buttonWidth = boxWidth - padding * 2;
+	
+	void OnGUI() {
+		
+		GUIStyle buttonStyle = new GUIStyle("button");
+		buttonStyle.fontSize = 32;
+		GUIStyle boxStyle = new GUIStyle("box");
+		boxStyle.fontSize = 38;
+		
+		GUI.Box(new Rect(padding, boxY, boxWidth, boxHeight), "Main Menu", boxStyle);
+		
+		
+		if(GUI.Button(new Rect(padding * 2, buttonHeight * 2 + padding + boxY, buttonWidth, buttonHeight), "Play Game", buttonStyle)) {
+			Application.LoadLevel("ClickArena");
+		}
+	}
+}
