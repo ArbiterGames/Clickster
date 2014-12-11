564fbb374732320e071c7482e76dcae955639c47
Andy Zinsser <andyznssr@gmail.com>
2014-12-10 18:47:20 -0800

    Add asset store skin

---
+++ Clickster/Assets/SpritePacker/Required/Player/SP_Result.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SP_Result
+{
+	[SerializeField]
+	private string identifier;
+	
+	[SerializeField]
+	private string name;
+	
+	[SerializeField]
+	private int index;
+	
+	[SerializeField]
+	private Sprite sprite;
+	
+	public string Identifier
+	{
+		set
+		{
+			identifier = value;
+		}
+		
+		get
+		{
+			return identifier;
+		}
+	}
+	
+	public string Name
+	{
+		set
+		{
+			name = value;
+		}
+		
+		get
+		{
+			return name;
+		}
+	}
+	
+	public int Index
+	{
+		set
+		{
+			index = value;
+		}
+		
+		get
+		{
+			return index;
+		}
+	}
+	
+	public Sprite Sprite
+	{
+		set
+		{
+			sprite = value;
+		}
+		
+		get
+		{
+			return sprite;
+		}
+	}
+}
